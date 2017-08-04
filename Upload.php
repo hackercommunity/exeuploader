01fbda71899a143e8f83c66e46ea770bb69564eb
mwear3robloxandroid <mitchellwears@gmail.com>
2017-08-04 17:25:34 -0400

    Add files via upload

---
+++ Upload.php
@@ -0,0 +1,27 @@
+<?php
+$target_dir = "uploads/exe";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual exe or fake exe
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an exe - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an exe.";
+        $uploadOk = 0;
+    }
+}
+?>
+<?php
+if ($handle = opendir('/uploads/exe')) {
+    while (false !== ($file = readdir($handle))) {
+        if ($file != "." && $file != "..") {
+            echo '<a href="http://hackercommunity.github.io/' . $file . '">' . $file . '</a><br />';
+        }
+    }
+    closedir($handle);
+}
+?>
